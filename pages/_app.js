854360926e4d3b4a08898f19e09bbbf815ddd6c5
giovanaandrade <giovanadandrade@gmail.com>
2021-01-28 17:49:35 -0300

    atualizando meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -32,24 +32,33 @@ const { theme } = db;
 
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
+  const title = 'InterQuiz - um quiz sobre o Sport Club Internacional';
+  const description = 'Esse quiz foi criado durante a Imersão React da Alura';
+  const image = '/images/inter6.jpg';
+  const url = 'https://interquiz.vercel.app/';
+
   return (
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
 
-        {/* <!-- Primary Meta Tags --> */}
-        <title>InterQuiz - um quiz sobre o Sport Club Internacional</title>
-        <meta name="title" content="InterQuiz - um quiz sobre o Sport Club Internacional" />
-        <meta name="description" content="Esse quiz foi criado durante a Imersão React da Alura" />
+        <title>{title}</title>
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
         <link rel="icon" href="/favicon.svg" />
 
-        {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://interquiz.vercel.app/" />
-        <meta property="og:title" content="InterQuiz - um quiz sobre o Sport Club Internacional" />
-        <meta property="og:description" content="Esse quiz foi criado durante a Imersão React da Alura" />
-        <meta property="og:image" content="/images/inter6.jpg" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={image} />
       </Head>
 
       <ThemeProvider theme={theme}>
